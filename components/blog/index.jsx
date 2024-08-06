78fa879cf95d417fe7bad8f6bd15cf37fc886a94
Hiep Nguyen <nxhiep.1996@gmail.com>
2024-08-06 09:33:33 +0700

    minor changes

--- components/blog/index.jsx
+++ components/blog/index.jsx
@@ -1,14 +1,10 @@
-import Image from 'next/image';
 import styles from './index.module.css';
 
 const Blog = ({ post }) => {
     return (
         <div className='container'>
             <div className={"flex flex-ai-start flex-jc-sb " + styles.item}>
-                <div style={{ flex: 1 }}>
-                    <div style={{ height: 30 }}></div>
-                    <Image src={post.image} alt={post.title} style={{ width: '100%' }} />
-                </div>
+                <div style={{ flex: 1, backgroundImage: `url(${post.image})` }} className={styles.image}></div>
                 <div style={{ width: 80 }}></div>
                 <div style={{ flex: 1 }}>
                     <h1>{post.title}</h1>
