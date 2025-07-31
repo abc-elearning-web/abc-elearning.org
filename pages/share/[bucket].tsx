68335b660a7f95fd59737d150c355d2b20265fd6
hiep.nx <hiepnx27@gmail.com>
2025-07-31 09:33:24 +0700

    minor changes

--- pages/share/[bucket].tsx
+++ pages/share/[bucket].tsx
@@ -30,8 +30,7 @@ const Share = ({ appConfig, query }: { appConfig: any, query: string }) => {
 };
 
 const getUserAgent = () => {
-    // return navigator.userAgent;
-    return "Mozilla/5.0 (iPhone; CPU iPhone OS 18_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.5 Mobile/15E148 Safari/604.1";
+    return navigator.userAgent;
 }
 
 const isIOS = () => {
